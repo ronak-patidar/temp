f2d360e207ada33a61d143982a986690f3a32174
ronak <patidarronak11@gmail.com>
2021-05-01 17:31:22 +0530

    changes done

---
+++ f.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include <cmath>
+
+using namespace std;
+//vector<int>dp(1001);
+void func(){
+      /*int p,i=1,j=0;
+      while(j<=100){
+          p=i*(floor(sqrt(i))+ceil(i/2));
+          while(p--){
+            dp[j++]=i;
+          }
+          i++;
+     }
+      
+      for(int i=0;i<=100;++i)
+          cout<<dp[i]<<" ";
+      cout<<endl;*/
+     int i=1;
+     cout<<ceil(i/2)<<endl;
+     
+  
+}
+int main(){
+    /*int n,t;
+    cin>>t;
+    while(t--){
+       cin>>n;
+       vector<int>a(n);
+       
+       for(int i=0;i<n;++i)
+         cin>>a[i];
+       cout<<func(a,n)<<endl;
+       
+    }*/
+    func();
+}
